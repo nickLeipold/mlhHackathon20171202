e4100c0ab05809b43fd572abc9bf15a180ccf5e4
Hugo Klepsch <hugo.klepsch@gmail.com>
2017-12-02 17:12:54 -0500

    Add schemas and newGame API first draft
    * schemas for
      - POST to /api/newgame

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 var express = require('express')
 var URL = require('url');
 
+var Validator = require('jsonschema').Validator;
+var postNewGameSchema = require('./schema/postNewGame');
+var postGameSessionSchema = require('./schema/postGameSession');
+var v = new Validator();
+
 var app = express();
 
 // In memory db for fast speed
-var db = [];
+var db = {};
 
 app.use(express.json());
 
@@ -19,21 +24,70 @@ app.use(function(req, res, next) {
 });
 
 // API
+app.get("/api/newgame", function(req, res, next) {
+    var sessionKey = newSessionKey();
+    res.send(sessionKey);
+
+    db[sessionKey] = {
+        gameState: 'lobby',
+        currentPlayer: 'w',
+        board: newBoard(),
+    };
+
+    next();
+});
+
 app.post("/api/newgame", function(req, res, next) {
 
+    responseJSON = {
+        status: 'failed',
+        yourPlayer: 'white',
+        board: encodeBoard(newBoard())
+    };
+
     if (req.body !== undefined) {
-        log({
-            message: 'API request received',
-            body: JSON.stringify(req.body)
-        });
-        log(req.body);
+        if (v.validate(req.body, postNewGameSchema)) {
+            log({
+                message: 'API request received',
+                body: JSON.stringify(req.body)
+            });
+
+            log({
+                message: 'Adding session: ' + req.body.sessionKey
+            });
+
+            if (req.body.sessionKey in db) {
+                responseJSON.board = encodeBoard(db[req.body.sessionKey]);
+                responseJSON.status = 'found';
+
+                log({
+                    message: 'Session found'
+                });
+            } else {
+                responseJSON.board = encodeBoard(newBoard());
+                responseJSON.status = 'not found';
+
+                log({
+                    message: 'Session not found',
+                    session: req.body.sessionKey
+                });
+            }
+        } else {
+            log({
+                message: 'Body failed validation',
+                body: JSON.stringify(req.body)
+            });
+        }
     } else {
         log({
             message: 'Body does not contain JSON'
         });
     }
 
-    res.send('hello world\n');
+
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(responseJSON));
 
     next();
 });
@@ -78,4 +132,37 @@ function log(obj) {
 
 function logBasic(logStr) {
     console.log(logStr);
+}
+
+function newSessionKey() {
+    var characterSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+    var sessionKey = '';
+
+    for (var i = 0; i < 5; ++i) {
+        sessionKey += characterSet[Math.floor(Math.random() * characterSet.length)];
+    }
+
+    return sessionKey;
+}
+
+function newBoard() {
+    var boardJSON = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';
+    return boardJSON;
+}
+
+function encodeBoard(board) {
+    var boardArray = [];
+    for (var i = 0; i < board.length; ++i) {
+        var ch = boardArray[i];
+
+        if (['r', 'n', 'b', 'q', 'k', 'p'].prototype.includes(ch.toLowerCase())) {
+            boardArray.push(ch);
+        } else if (!isNan(ch)) {
+            for (var j = 0; j < parseInt(ch); ++i) {
+                boardArray.push(' ');
+            }
+        } else {
+        }
+    }
+    return boardArray;
 }
