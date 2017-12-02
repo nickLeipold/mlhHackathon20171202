8d483d30599e896d8790bb7dfdc7b87182542925
Grant <grantdouglashand@outlook.com>
2017-12-02 15:07:12 -0500

    upload now adds web files to var/www/html

--- upload.sh
+++ upload.sh
@@ -5,6 +5,8 @@ SERVER=webaccess@orion.hugo-klepsch.tech
 echo $DIR
 
 rsync -avz --exclude=/mlhHackathon20171202/.git/ $DIR $SERVER:/home/webaccess
+rsync -avz --exclude=/mlHackathon20171202/.git/ $DIR/web/* $SERVER:/var/www/html
+
 
 
 
