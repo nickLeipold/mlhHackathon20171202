e08195e3b3dde803e3f76c281aba2990b251869e
Hugo Klepsch <hugo.klepsch@gmail.com>
2017-12-02 20:49:07 -0500

    fix upload script

--- upload.sh
+++ upload.sh
@@ -5,7 +5,7 @@ SERVER=webaccess@orion.hugo-klepsch.tech
 echo $DIR
 
 rsync -avz --exclude=/mlhHackathon20171202/.git/ $DIR $SERVER:/home/webaccess
-rsync -avz --exclude=/mlHackathon20171202/.git/ $DIR/web/* $SERVER:/var/www/html
+#rsync -avz --exclude=/mlHackathon20171202/.git/ $DIR/web/* $SERVER:/var/www/html
 
 
 
